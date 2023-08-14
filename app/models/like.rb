29e020a4499f59830018ca9a1df0047de772ec69
cherelemma <chere.lemma@aastu.edu.et>
2023-08-14 17:51:41 +0300

    add association between like and post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :
-  
+  belongs_to :post
+
   after_create :update_likes_counter
   after_destroy :update_likes_counter
 
