6965b5bce06701068c9566a05926d3c95bbd619e
cherelemma <chere.lemma@aastu.edu.et>
2023-08-14 17:21:13 +0300

    create Post model and add necessary association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,24 @@
 class Post < ApplicationRecord
-  belongs_to :author_id
+  belongs_to :author_id , class_name: "User" , foreign_key: "author_id" , counter_cache: :posts_counter
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  after_save :update_posts_counter
+  after_destroy :update_posts_counter
+  
+  def update_posts_counter
+    if destroyed?
+      author.decrement!(:posts_counter)
+      return
+    end
+    author.increment!(:posts_counter)
+  end
+  
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
+
+  def recent_likes(limit = 3)
+    likes.order(created_at: :desc).limit(limit)
+  end
 end
