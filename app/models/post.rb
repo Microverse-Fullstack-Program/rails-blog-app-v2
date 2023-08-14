b2edec8d514e004942f2227e6c459a5144992ba7
cherelemma <chere.lemma@aastu.edu.et>
2023-08-14 17:45:05 +0300

    fix rubocop error

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
 
   after_save :update_posts_counter
   after_destroy :update_posts_counter
-  
+
   def update_posts_counter
     if destroyed?
       author.decrement!(:posts_counter)
@@ -13,7 +13,7 @@ class Post < ApplicationRecord
     end
     author.increment!(:posts_counter)
   end
-  
+
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
