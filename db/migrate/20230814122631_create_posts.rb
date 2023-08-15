20e838ab9e9fa3b6da950deeeb8dafcadff0c47c
cherelemma <chere.lemma@aastu.edu.et>
2023-08-15 15:52:53 +0300

    remove wrongly added validation form Posts migration

--- db/migrate/20230814122631_create_posts.rb
+++ db/migrate/20230814122631_create_posts.rb
@@ -12,8 +12,4 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     add_foreign_key :posts, :users, column: :author_id
     add_index :posts, :author_id
   end
-
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 end
