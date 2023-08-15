a2a259ac808cc7da7369b2b3856f22b7a7a316f2
cherelemma <chere.lemma@aastu.edu.et>
2023-08-15 15:49:56 +0300

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }  
 end
