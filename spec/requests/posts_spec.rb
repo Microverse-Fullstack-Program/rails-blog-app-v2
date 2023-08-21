fe05191d230cd8c4915905b6317b42a1014ac4aa
cherelemma <chere.lemma@aastu.edu.et>
2023-08-21 13:11:05 +0300

    add validation: check the response status was correct

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      get '/posts/index'
+      expect(response.status).to eq(200
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/posts/show'
+      expect(response.status).to eq(200
+    end
   end
 end
