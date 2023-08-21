446781f4abac783ada0bafaea52fa864e56ef3a7
cherelemma <chere.lemma@aastu.edu.et>
2023-08-21 16:58:13 +0300

    Create a Request spec for Posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,41 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
+  describe 'GET posts#index' do
     it 'returns http success' do
-      get '/posts/index'
-      expect(response.status).to eq(200
+      get '/users/1/posts'
+      expect(response.status).to eq(200)
     end
   end
 
-  describe 'GET /show' do
+  describe 'GET posts#show' do
     it 'returns http success' do
-      get '/posts/show'
-      expect(response.status).to eq(200
+      get user_post_path(1, 1)
+      expect(response.status).to eq(200)
     end
   end
 
   describe 'Template rendering' do
-    it 'renders the index template' do
-      get '/posts/index'
+    it 'renders the /posts#index template' do
+      get user_posts_path(1)
       expect(response).to render_template('index')
     end
 
-    it 'renders the show template' do
-      get '/posts/show'
+    it 'renders the /posts#show template' do
+      get user_post_path(1, 1)
       expect(response).to render_template('show')
     end
   end
 
   describe 'response body' do
-    it 'contains correct /Posts#index/ palceholder text' do
-      get '/posts/index'
-      expect(response.body).to match(/Posts#index/)
+    it 'contains correct /posts#index/ palceholder text' do
+      get user_posts_path(1)
+      expect(response.body).to include('Here is a list of posts with all comments for a given user')
     end
 
-    it 'contains correct /Posts#show/ palceholder text' do
-      get '/posts/show'
-      expect(response.body).to match(/Posts#show/)
+    it 'contains correct /posts#show/ palceholder text' do
+      get user_post_path(1, 1)
+      expect(response.body).to include('Here is a post with list of comments for a given user')
     end
   end
 end
