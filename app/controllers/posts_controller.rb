1c569fa9755b50b50388497c5b2344f6313f8638
cherelemma <chere.lemma@aastu.edu.et>
2023-08-22 23:32:12 +0300

    add @current_user to Posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @current_user = current_user
     @user = User.find(params[:user_id])
 
     redirect_to root_path if @user.nil?
@@ -14,5 +15,29 @@ class PostsController < ApplicationController
 
     redirect_to user_posts_path(@user) if @post.nil?
     @comments = @post.comments
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+
+    redirect_to root_path if @user.nil?
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+
+    if @post.save
+      redirect_to user_posts_path, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
