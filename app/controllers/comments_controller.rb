6638ea1a458479289640f4cb68a32c4d05c89cb5
cherelemma <chere.lemma@aastu.edu.et>
2023-08-22 23:28:12 +0300

    create Comments_controller and add action methods

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+  def new 
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author_id = current_user.id
+    @comment.post_id = params[:post_id]
+
+    if @comment.save
+      redirect_to user_post_path(author_id: @comment.author_id, id: @comment.post_id),
+                  notice: 'Comment was successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
