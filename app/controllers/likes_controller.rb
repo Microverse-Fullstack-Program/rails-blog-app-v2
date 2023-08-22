426e3727d45fbd77346b51817862a3b4330b5a97
cherelemma <chere.lemma@aastu.edu.et>
2023-08-22 23:28:35 +0300

    create Likes_controller and add action methods

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,13 @@
+class LikesController < ApplicationController
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.create(author_id: current_user.id)
+    @like.save
+    
+    redirect_to user_post_path(@post.author_id, @post.id)
+  end
+end
