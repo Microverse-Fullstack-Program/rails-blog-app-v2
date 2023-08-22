64aeaec97f1c9b9f3f652de4c49def40915c509d
cherelemma <chere.lemma@aastu.edu.et>
2023-08-22 23:39:06 +0300

    debug and fix linter with rubocop

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,7 +7,7 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @like = @post.likes.create(author_id: current_user.id)
     @like.save
-    
+
     redirect_to user_post_path(@post.author_id, @post.id)
   end
 end
