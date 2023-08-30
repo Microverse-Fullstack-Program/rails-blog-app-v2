cd24fe3fdea1a06b1408fa7e57ce3c4329e1676c
cherelemma <chere.lemma@aastu.edu.et>
2023-08-30 18:03:23 +0300

    Add  index and show action to Comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  def index; end
+  def show; end
+  
   def new
     @comment = Comment.new
     @post = Post.find(params[:post_id])
@@ -15,6 +18,18 @@ class CommentsController < ApplicationController
                   notice: 'Comment was successfully created.'
     else
       render 'new'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+
+    if can? :destroy, @comment
+      @comment.destroy
+      redirect_to user_post_path(@post.author, @post), notice: 'Comment was successfully deleted.'
+    else
+      redirect_to user_posts_path(current_user), alert: 'You are not authorized to delete this comment.'
     end
   end
 
