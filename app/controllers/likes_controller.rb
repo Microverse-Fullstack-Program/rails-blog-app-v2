00eaff8d10fbf9c1d04e36aeb458a6e144ba115e
cherelemma <chere.lemma@aastu.edu.et>
2023-09-01 08:24:57 +0300

    add posts dislike functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,9 +5,14 @@ class LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @like = @post.likes.create(author_id: current_user.id)
-    @like.save
+    if Like.where(author_id: current_user.id, post_id: @post.id).exists?
+      @like = Like.find_by(author_id: current_user.id, post_id: @post.id)
+      @like.destroy
+    else
+      @like = @post.likes.create(author_id: current_user.id)
+      @like.save
+    end
 
-    redirect_to user_post_path(@post.author_id, @post.id)
+    redirect_to request.referrer
   end
 end
